bbb101ab8a6ad618aa6812f4028b12bab407b10a
wendy <wendycaner@gmail.com>
2016-02-25 15:01:53 +0800

    Can use relative path "/"

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-
+var replace = require('gulp-replace');
 var sass = require('gulp-sass');
-
 var concat = require('gulp-concat');
+
+var baseUrl = 'file://' + __dirname + '/';
 
 const paths = {
   bundle: 'main.js',
-  srcJsWatched: './javascripts/*',
+  srcHtml: 'html/*.html',
+  srcJsWatched: './javascripts/*.coffee',
   srcJs: 'javascripts/main.coffee',
   srcCss: 'stylesheets/main.scss',
   dist: 'src'
@@ -30,11 +32,20 @@ gulp.task('coffee', function () {
 gulp.task('sass', function () {
   gulp.src(paths.srcCss)
     .pipe(concat('main.css'))
+    .pipe(replace(/@import "[\/]+/g, "@import \"" + baseUrl))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.dist));
 });
 
+gulp.task('html', function () {
+  gulp.src(paths.srcHtml)
+    .pipe(replace(/src="[\/]+/g, "src=\"" + baseUrl))
+    .pipe(replace(/href="[\/]+/g, "href=\"" + baseUrl))
+    .pipe(gulp.dest(paths.dist));
+})
+
 gulp.watch(paths.srcJsWatched, ['coffee']);
 gulp.watch(paths.srcCss, ['sass']);
+gulp.watch(paths.srcHtml, ['html']);
 
-gulp.task('default', ['coffee', 'sass']);
+gulp.task('default', ['coffee', 'sass', 'html']);
